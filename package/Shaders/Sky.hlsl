f03f7217a2097f4605a9660bc22df4be769386cf
jiayev <l936249247@hotmail.com>
2026-05-15 14:38:37 +0800

    fix(ll): reduce sky dither noise (#2342)
    (cherry picked from commit 6c068bef7f9d7542c3790d9f3dad1ea1fd697e7c)

--- package/Shaders/Sky.hlsl
+++ package/Shaders/Sky.hlsl
@@ -221,10 +221,13 @@ PS_OUTPUT main(PS_INPUT input)
 		TexNoiseGradSampler.Sample(SampNoiseGradSampler, noiseGradUv).x * 0.03125 + -0.0078125;
 
 #			ifdef TEX
-	psout.Color.xyz = (Color::Sky(input.Color.xyz) * baseColor.xyz + yyy) + noiseGrad;
+	float3 skyVertColor = ENABLE_LL ? (input.Color.xyz + noiseGrad) : input.Color.xyz;
+	float3 sunGlareColor = Color::Sky(skyVertColor) * baseColor.xyz;
+	// Dither/noise term is the legacy sky path contribution for gradient smoothing.
+	psout.Color.xyz = (sunGlareColor + yyy) + (ENABLE_LL ? 0.0 : noiseGrad);
 	psout.Color.w = baseColor.w * input.Color.w;
 #			else
-	psout.Color.xyz = (yyy + Color::Sky(input.Color.xyz)) + noiseGrad;
+	psout.Color.xyz = yyy + Color::Sky(input.Color.xyz + noiseGrad);
 	psout.Color.w = input.Color.w;
 #			endif  // TEX
 

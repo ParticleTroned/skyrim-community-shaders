a6840d22d0e7c44e50e168418f6961d669235093
ParticleTroned <248299730+ParticleTroned@users.noreply.github.com>
2026-05-18 02:25:50 +0100

    fix(upscaling): reset FSR4 foveated runtime state

--- src/Features/Upscaling/FidelityFX.cpp
+++ src/Features/Upscaling/FidelityFX.cpp
@@ -750,6 +750,14 @@ void FidelityFX::DestroyRuntimeUpscalerResources(bool a_waitForIdle)
 	runtimeOutputSharedDesc = {};
 }
 
+void FidelityFX::ResetRuntimeUpscalerResources()
+{
+	WaitForRuntimeUpscalerIdle();
+	DestroyRuntimeUpscalerContexts(false);
+	DestroyRuntimeUpscalerResources(false);
+	ResetRuntimeUpscalerTracking(false);
+}
+
 void FidelityFX::DestroyFSRResources()
 {
 	const uint32_t numContexts = fsrContextCount;

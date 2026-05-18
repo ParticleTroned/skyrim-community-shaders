f973f58c0d9542bc9c343e590a288203ab8e26bc
ColdBomb1 <ColdBomb1@gmail.com>
2026-05-18 00:34:38 -0400

    fix(upscaling): keep VR dynamic resolution stable

--- src/Features/Upscaling.cpp
+++ src/Features/Upscaling.cpp
@@ -980,6 +980,65 @@ namespace
 		changedByUpscaling = a_enabled;
 	}
 
+	void DisableAutoDynamicResolutionSetting()
+	{
+		if (!globals::game::isVR)
+			return;
+
+		constexpr const char* settingNames[] = {
+			"bEnableAutoDynamicResolution:Display",
+			"bEnableAutoDynamicResolution"
+		};
+
+		bool found = false;
+		bool changed = false;
+		auto disableInCollection = [&](auto* a_collection, const char* a_collectionName) {
+			if (!a_collection)
+				return;
+
+			for (const auto* settingName : settingNames) {
+				auto* setting = a_collection->GetSetting(settingName);
+				if (!setting)
+					continue;
+
+				found = true;
+				if (setting->data.b) {
+					setting->data.b = false;
+					changed = true;
+					if (a_collection->WriteSetting(setting)) {
+						logger::info("[Upscaling] Disabled {} in {}.", settingName, a_collectionName);
+					} else {
+						logger::warn("[Upscaling] Disabled {} in memory, but failed to write {}.", settingName, a_collectionName);
+					}
+				}
+				return;
+			}
+		};
+
+		disableInCollection(globals::game::iniSettingCollection, "Skyrim.ini");
+		disableInCollection(globals::game::iniPrefSettingCollection, "SkyrimPrefs.ini");
+
+		for (const auto* settingName : settingNames) {
+			auto* setting = RE::GetINISetting(settingName);
+			if (!setting)
+				continue;
+
+			found = true;
+			if (setting->data.b) {
+				setting->data.b = false;
+				changed = true;
+				logger::info("[Upscaling] Disabled {} in runtime settings.", settingName);
+			}
+			break;
+		}
+
+		if (!found) {
+			logger::debug("[Upscaling] bEnableAutoDynamicResolution setting was not found.");
+		} else if (!changed) {
+			logger::debug("[Upscaling] bEnableAutoDynamicResolution was already disabled.");
+		}
+	}
+
 	bool IsVRRuntimeActive()
 	{
 		return globals::game::isVR;
@@ -2086,6 +2145,7 @@ struct BSOpenVR_GetRenderTargetSize
 
 void Upscaling::DataLoaded()
 {
+	DisableAutoDynamicResolutionSetting();
 	ApplyOpenCompositeUpscalingBlocker(true);
 	const auto& blocker = GetOpenCompositeUpscalingBlocker();
 	if (blocker.active) {
@@ -5196,9 +5256,13 @@ void Upscaling::ApplyDynamicResolutionState(RE::BSGraphics::State* a_viewport)
 	if (globals::game::isVR) {
 		SetDynamicResolutionEnabledForUpscaling(shouldUnlockDynamicResolution);
 		if (shouldUnlockDynamicResolution) {
+			runtimeData.dynamicResolutionPreviousWidthRatio = runtimeData.dynamicResolutionWidthRatio;
+			runtimeData.dynamicResolutionPreviousHeightRatio = runtimeData.dynamicResolutionHeightRatio;
+			runtimeData.dynamicResolutionWidthRatio = resolutionScale.x;
+			runtimeData.dynamicResolutionHeightRatio = resolutionScale.y;
 			runtimeData.dynamicResolutionLock = 0;
-			dynamicResolutionWidthRatio = runtimeData.dynamicResolutionWidthRatio;
-			dynamicResolutionHeightRatio = runtimeData.dynamicResolutionHeightRatio;
+			dynamicResolutionWidthRatio = resolutionScale.x;
+			dynamicResolutionHeightRatio = resolutionScale.y;
 		} else {
 			runtimeData.dynamicResolutionPreviousWidthRatio = 1.0f;
 			runtimeData.dynamicResolutionPreviousHeightRatio = 1.0f;
@@ -5431,17 +5495,6 @@ void Upscaling::PostDisplay()
 
 	viewport->projectionPosScaleX = projectionPosScaleX;
 	viewport->projectionPosScaleY = projectionPosScaleY;
-
-	auto& runtimeData = viewport->GetRuntimeData();
-
-	runtimeData.dynamicResolutionPreviousWidthRatio = 1;
-	runtimeData.dynamicResolutionPreviousHeightRatio = 1;
-	runtimeData.dynamicResolutionWidthRatio = 1;
-	runtimeData.dynamicResolutionHeightRatio = 1;
-	runtimeData.dynamicResolutionLock = 1;
-
-	globals::game::renderer->UpdateViewPort(0, 0, 1);
-	UpdateCameraData();
 
 	if (d3d12SwapChainActive)
 		SetUIBuffer();
